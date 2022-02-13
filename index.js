bf4dd018f62aa6497ce574244d274ff50d603ba7
Alex <alex@letris.ru>
2022-02-13 15:45:12 +0800

    checksum bug fixed

--- index.js
+++ index.js
@@ -1509,6 +1509,15 @@ class App extends EventEmitter {
         res = stream.uint32(res.offset);
         let checksum = parseInt(res.result).toString(16);
 
+        if (checksum.length == 2)
+            checksum = "000000" + checksum;
+
+        if (checksum.length == 4)
+            checksum = "0000" + checksum;
+
+        if (checksum.length == 6)
+            checksum = "0000" + checksum;
+
         //version 1 - version|checksum|filter_to|encrypteddata
         //version 2 - version|checksum|filter_from|filter_to|encrypteddata
         //version 3 - version|checksum|pubkey_to|encrypteddata //unsafe
